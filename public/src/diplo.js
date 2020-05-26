13f8ce0cd5fd994505fa7973e8a687ca4ecb1e86
duchoprog <duchoprog@gmail.com>
2020-05-26 07:56:17 -0300

    arranco2

--- public/src/diplo.js
+++ public/src/diplo.js
@@ -5,15 +5,23 @@ function sendMessage(){
 function signInOrUp(){
     mail=document.getElementById("mail").value
     console.log(mail)
+    name=document.getElementById("name").value
+    console.log(name)
     pass=document.getElementById("pass").value
     console.log(pass)
-    firebase.auth().signInWithEmailAndPassword(mail, pass).catch(function(error) {
+    firebase.auth().signInWithEmailAndPassword(mail, pass).then(function(e){
+      console.log(e.user.email)
+    })
+    .catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(errorMessage)
 
-        firebase.auth().createUserWithEmailAndPassword(mail, pass).catch(function(error) {
+        firebase.auth().createUserWithEmailAndPassword(mail, pass).then(function(e){
+          console.log(e)
+        })
+        .catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
